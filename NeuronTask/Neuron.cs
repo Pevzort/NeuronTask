bf98b1907acdeb84c8a82c819eab6953447db3cc
Pevzort Pevzort <maslov.mr@yandex.ru>
2022-04-07 16:57:03 +0300

    Реализованна базовая структура нейронной сети(БЕЗ ОБУЧЕНИЯ)

--- NeuronTask/Neuron.cs
+++ NeuronTask/Neuron.cs
@@ -40,6 +40,15 @@ namespace NeuronTask
             return 1.0 / (1.0 + Math.Exp(-x));
         }
 
+        public void SetWeights(params double[] weights)
+        {
+            //TODO: удалить после добавление возможности обучения сети
+            for(int i = 0; i < weights.Length; i++)
+            {
+                Weights[i] = weights[i];
+            }
+        }
+
         public override string ToString()
         {
             return Output.ToString();
